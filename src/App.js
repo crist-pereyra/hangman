a43c106816e89d38cc30fb944d7ee72a925357af
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2021-08-05 15:37:04 -0400

    Create Figure component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './comps/Header';
+import Figure from './comps/Figure';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="game-container">
+        <Figure />
+      </div>
     </>
   );
 }
