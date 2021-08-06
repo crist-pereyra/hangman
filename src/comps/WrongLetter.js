3f8396f20a7a75b9a99fc11cabcbf410c90c1a07
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2021-08-05 23:40:15 -0400

    Event Listener with useEffect

--- src/comps/WrongLetter.js
+++ src/comps/WrongLetter.js
@@ -6,8 +6,8 @@ export const WrongLetter = ({wrongLetters}) => {
             <div>
                 {wrongLetters.length > 0 && <p>Wrong</p>}
                 {wrongLetters
-                .map((letter, i) => <span key={i}>{letter}</span>)
-                .reduce((prev, curr) => prev === null ? [curr] : [prev, ', ',curr], null)}
+                    .map((letter, i) => <span key={i}>{letter}</span>)
+                    .reduce((prev, curr) => prev === null ? [curr] : [prev, ', ',curr], null)}
             </div>
         </div>
     )
